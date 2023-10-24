40b90d6a21dd160e09c13f60615e96a9f35787d7
Vivek <vivek.rbj.up@gmail.com>
2023-10-24 18:58:26 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = ()=>{
+    return (
+        <>
+            <nav>
+                <img className="logo" src="https://cuvette.tech/app/static/media/logo.74bda650.svg" alt="cuvette logo" />
+                <div className="profile">
+                    <img className="profile_img" src="https://cuvette.tech/app/static/media/student17.61c07a31.png" alt="profile img" />
+                    <span className="name">Siddhart Jain</span>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
